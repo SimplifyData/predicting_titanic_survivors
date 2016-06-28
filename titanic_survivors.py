ba79ac2583cfd9286e1cef9aa600082daeb79141
simplifydata <atif.zafar@sds.technology>
2016-06-28 00:42:45 -0700

    analyzing posterior distribution of men surviving in different pclasses

--- titanic_survivors.py
+++ titanic_survivors.py
@@ -1,4 +1,5 @@
 import dask.dataframe as dd
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
@@ -11,13 +12,75 @@ def read_csv(file):
     This function reads the csv file
     :return:
     """
+    #file = raw_input("csv file path")
     return pd.read_csv(file)
 
 def Survivors():
 
     training_data = read_csv('/home/azafar/Projects/predicting_titanic_survivors/data/train.csv')
 
-    print training_data
+    print training_data.head()
+
+    # metrics of men who survived
+
+    men_high_fare = training_data[["Fare","Survived", "Sex","Pclass"]][training_data["Sex"]=="male"][training_data["Survived"]==1]
+
+    print men_high_fare.describe()
+
+    #metrics of male in pclasses
+
+    men_pclass = training_data["Pclass"][training_data["Sex"]=="male"].describe()
+
+    #total counts for males in different pclass
+
+    men_pclass_counts = training_data["Pclass"][training_data["Sex"] == "male"].value_counts()
+
+    #metrics of male in classes who survived
+
+    men_class_survival = training_data["Pclass"][training_data["Sex"] == "male"][training_data["Survived"]==1].describe()
+
+    # counts of male in pclasses who survived
+
+    men_class_survival_counts = training_data["Pclass"][training_data["Sex"] == "male"][
+        training_data["Survived"] == 1].value_counts()
+
+    print "\n metrics of men who survived"
+
+    print men_pclass
+
+    print "\n metrics of male in pclasses"
+
+    print men_class_survival
+
+    print "\n total counts for males in different pclass"
+
+    print men_pclass_counts
+
+    print "\n counts of male in pclasses who survived"
+
+    print men_class_survival_counts
+
+    print "\n Men Survival ratio in different pclasses"
+
+    men_survival_ratio = (men_class_survival_counts / men_pclass_counts)
+
+    print men_survival_ratio
+
+    # Posterior distribution of men surviving in different classes
+
+    print "\n Posterior distribution of men who survived in different pclasses"
+
+    print (men_survival_ratio / men_survival_ratio.sum())
+
+    men_class_survival_counts.plot(kind = "hist")
+
+
+
+
+
+
+
+
 
 
 
