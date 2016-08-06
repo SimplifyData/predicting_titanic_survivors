b643af5bc71ca128bb80e73c2408fef272632592
simplifydata <atif.zafar@sds.technology>
2016-08-05 22:36:14 -0700

    number of people surviving by gender

--- titanic_survivors.py
+++ titanic_survivors.py
@@ -95,9 +95,14 @@ def Survivors():
 
     plt.show()
 
+    training_data['SibSp'].value_counts().plot.bar()
+
+    plt.show()
+
     training_data['Parch'].value_counts().plot.bar()
 
     plt.show()
+
 
     # group by mean value of the people who survived
 
@@ -107,13 +112,67 @@ def Survivors():
 
     print groupby_survived
 
-    # pasangers survival rate accroding tot he pclass
-    
+    # pasangers survival rate accroding to the pclass
+
     pclass_survived = training_data[["Pclass", "Age", "Fare", "Survived"]].groupby("Pclass").mean()
 
-    print "pasanger survival rate accorinf tot he survival rate"
+    print "\n passenger survival rate according to the survival rate"
 
     print pclass_survived
+
+    sum_survived_bysex = training_data[["Sex","Age","Pclass","Fare","Survived"]].groupby("Sex").mean()
+
+    print "\n total people analyzed by thier sex"
+
+    print sum_survived_bysex
+
+    # total survived by seen by thier sex
+
+    survived_bysex = training_data[["Sex", "Survived"]].groupby("Sex").mean()
+
+    print " \n total survived seen by thier sex"
+
+    print survived_bysex
+
+    #value counts of survivals
+
+    num_ppl_survived = training_data["Survived"].value_counts()
+
+    print "\n value counts of survived people"
+
+    print num_ppl_survived
+
+
+
+    #total number of females and males
+
+    sum_f_m = training_data["Sex"].value_counts()
+
+    print "\n total number of males and females"
+
+    print sum_f_m
+
+    # number of females and males surviving
+
+    f_m_survived = (sum_f_m[1] * survived_bysex._get_values[0], sum_f_m[0] * survived_bysex._get_values[1])
+
+    print "\n number of female and males surviving 1) !=survived 2) survived"
+
+    print f_m_survived
+
+    print "\n % of females and males surviving - checked"
+
+    percentage_f_m_survived = f_m_survived[0] / sum_f_m[1], f_m_survived[1] / sum_f_m[0]
+
+
+
+
+
+
+
+
+
+
 
 
 
